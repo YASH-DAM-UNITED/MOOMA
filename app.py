24e5da50b3b03d0dddc37c2ea71dbc26cd52bbfa
YASH <data@damunited.sa>
2026-06-14 13:08:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if "lang" not in st.session_state: st.session_state.lang = "en"
 # Define your texts
 texts = {
     "en": {
-        "title": "G L O R ",
+        "title": "M O O M A",
         "sub": "Operations management <br>just got easier.",
         "desc": "Welcome to the central command unit for MOOMA...",
         "btn_staff": "Staff Access →"
