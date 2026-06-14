9935285af4343d499fc14fe07fab240f28f40698
YASH <data@damunited.sa>
2026-06-14 13:31:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ div.stButton > button {
 div.stButton > button:hover {
     transform: scale(1.05) translateY(-2px);
     background: #e3a857 !important; 
-    box-shadow: 0 10px 20px rgba(100, 13, 20, 0.4) !important;
+    box-shadow: 0 10px 20px rgba(227, 168, 87, 1.0) !important;
     letter-spacing: 4px !important;
 }
 div.stButton > button:active { transform: scale(0.98) translateY(0); }
@@ -144,7 +144,7 @@ div.stButton > button:active { transform: scale(0.98) translateY(0); }
 /* Keyframes Baseline */
 @keyframes spin { from { transform: translate(-50%, -50%) rotate(0deg); } to { transform: translate(-50%, -50%) rotate(360deg); } }
 @keyframes rotate { 100% { transform: rotate(360deg); } }
-@keyframes breathe-bold { 0%, 100% { transform: scale(1); text-shadow: 0 0 10px rgba(100, 13, 20, 0.4); } 50% { transform: scale(1.05); text-shadow: 0 0 30px (247, 93, 89, 0.65); } }
+@keyframes breathe-bold { 0%, 100% { transform: scale(1); text-shadow: 0 0 10px rgba(227, 168, 87, 1.0); } 50% { transform: scale(1.05); text-shadow: 0 0 30px (247, 93, 89, 0.65); } }
 @keyframes fadeInUp { 0% { opacity: 0; transform: translateY(30px); } 100% { opacity: 1; transform: translateY(0); } }
 .animate-text { animation: fadeInUp 0.8s ease-out forwards; opacity: 0; }
 .delay-1 { animation-delay: 0.2s; } .delay-2 { animation-delay: 0.4s; } .delay-3 { animation-delay: 0.6s; } .delay-4 { animation-delay: 0.8s; }
