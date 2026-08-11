159a620806fc723f93764d9fa2672c98ac09f6de
YASH <data@damunited.sa>
2026-08-11 15:38:46 +0300

    Update staff_dashboard.py

--- pages/staff_dashboard.py
+++ pages/staff_dashboard.py
@@ -12,7 +12,7 @@ import pandas as pd
 import time
 
 # ---------------- PAGE CONFIG ----------------
-st.set_page_config(layout="wide", page_title="GLOR Staff Dashboard")
+st.set_page_config(layout="wide", page_title="MOOMA Staff Dashboard")
 
 SESSION_TIMEOUT = 30 * 60
 
@@ -50,7 +50,7 @@ st.markdown("""
     text-align: center;
     margin-bottom: 20px;
 ">
-<h1 style='color:white; margin:0;'>GLOR Staff Dashboard</h1>
+<h1 style='color:white; margin:0;'>MOOMA Staff Dashboard</h1>
 <p style='color:#e0e0e0; margin:0;'>Select Branch & Access Operations</p>
 </div>
 """, unsafe_allow_html=True)
